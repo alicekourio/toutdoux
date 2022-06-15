5ca261cbe507e80fb5a9b573612c462b0146a8cd
alicekourio <alice.kourio@spendesk.com>
2022-06-15 16:09:10 +0200

    refactor: move controller logic from router

--- src/http/routes/api.ts
+++ src/http/routes/api.ts
@@ -5,32 +5,8 @@ import {
   FastifyReply,
   FastifyRequest,
 } from 'fastify';
-import * as z from 'zod';
-import { v1 } from 'uuid';
-import sqlite3 from 'sqlite3';
-import { open, Database } from 'sqlite';
-import path from 'path';
 
-const sqlite3Verbose = sqlite3.verbose();
-
-async function openDb(): Promise<
-  Database<sqlite3.Database, sqlite3.Statement>
-> {
-  return open({
-    filename: path.join(process.cwd(), 'database/database.sqlite'),
-    driver: sqlite3.Database,
-  });
-}
-
-interface Todo {
-  id: string;
-  title: string;
-  completed: boolean;
-}
-
-const createTodoBodySchema = z.object({
-  title: z.string(),
-});
+import * as todoController from '../controllers/todo';
 
 export function apiRoutes(
   server: FastifyInstance,
@@ -40,42 +16,7 @@ export function apiRoutes(
   server.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
     reply.status(200).send({ hello: 'world' });
   });
-  server.post(
-    '/todos',
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      // console.log(request.body);
-      // // const title = request.body;
-      // console.log(title);
-      const parseResult = createTodoBodySchema.safeParse(request.body);
-      if (parseResult.success === false) {
-        reply.status(400).send(parseResult.error);
-        return;
-      }
-      // create new object as Todo base on request.body
-
-      const todoToInsert: Todo = {
-        id: v1(),
-        title: parseResult.data.title,
-        completed: false,
-      };
-
-      // send data in DB
-
-      const db = await openDb();
-
-      const result = await db.run(
-        'INSERT INTO todo(id, title, completed) VALUES (:id, :title, :completed)',
-        {
-          ':id': todoToInsert.id,
-          ':title': todoToInsert.title,
-          ':completed': todoToInsert.completed,
-        }
-      );
-
-      // then return it
-      return reply.status(200).send(todoToInsert);
-    }
-  );
+  server.post('/todos', todoController.createTodo);
 
   done();
 }
